4703408dc9bb3fb70d30847ff339dd05753ae324
Tiago Alves <alvesjtiago@gmail.com>
2018-12-20 02:10:03 +0000

    Initial Commit

---
+++ Recall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Recall
+//
+//  Created by Tiago Alves on 20/12/2018.
+//  Copyright © 2018 Recall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
